7994af0338c15cfcc83c65e6931940510e5a50e1
Jonkor <juan.kucho@hotmail.com>
2026-01-20 14:13:57 -0800

    Add form buttons

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -23,6 +23,7 @@ export const PostList = ({ isPosting,  onStopPosting}) => {
         <NewPost 
           onBodyChange={bodyChangeHandler} 
           onAuthorChange={authorChangeHandler}
+          onCancel={onStopPosting}
         />
       </Modal> 
       )}
