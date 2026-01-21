ad3dc6f01c975756e6ae714447ba3aa204dbf2a8
Jonkor <juan.kucho@hotmail.com>
2026-01-20 19:20:48 -0800

    Add form submission handle

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -5,31 +5,18 @@ import Modal from './Modal';
 import classes from './PostsList.module.css';
 
 export const PostList = ({ isPosting,  onStopPosting}) => {
-  const [enteredBody, setEnteredBody] = useState('');
-  const [enteredAuthor, setEnteredAuthor] = useState('');  
-  
-  const bodyChangeHandler = (event) => {
-    setEnteredBody(event.target.value);
-  };
-
-  const authorChangeHandler = (event) => {
-    setEnteredAuthor(event.target.value);
-  };
 
   return (
     <>
       {isPosting && 
       (<Modal onClose={onStopPosting}>
         <NewPost 
-          onBodyChange={bodyChangeHandler} 
-          onAuthorChange={authorChangeHandler}
           onCancel={onStopPosting}
         />
       </Modal> 
       )}
 
       <ul className={classes.posts}>
-        <li><Post author={enteredAuthor} body={enteredBody}/></li>
         <li><Post author="Manuel" body="Check out the full course"/></li>
       </ul>
     </>  
